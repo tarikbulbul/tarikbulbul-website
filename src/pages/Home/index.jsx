a27b6e01f2b555973eb612a81984257623eeccf7
Tarik Bulbul <bulbultarik@hotmail.com>
2023-08-26 06:02:48 +0300

    Add Home Component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import homeLogo from '../../assets/home-main.svg';
+import Contact from '../../components/Contact';
+import Type from '../../components/Type';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> TARIK BULBUL</strong>
+              </h1>
+
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Contact />
+    </section>
   )
 }
 
