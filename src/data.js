d81945ad3b2756d80447ff154961c9a96abe26d8
Tarik Bulbul <bulbultarik@hotmail.com>
2023-08-26 04:48:59 +0300

    initialize the project and create folder structure

---
+++ src/data.js
@@ -0,0 +1,68 @@
+import AirbnbClone from "./assets/Projects/airbnb-clone.png";
+import ReactMovieApp from "./assets/Projects/react-movie-app.png";
+import ReactBudgetApp from "./assets/Projects/react-budget-app.png";
+import NetflixClone from "./assets/Projects/netflix-clone.png";
+import RealEstateWeb from "./assets/Projects/real-estate.png";
+import DefiWeb from "./assets/Projects/defi-website.png";
+import TravelWeb from "./assets/Projects/travel-website.png";
+
+const projects = [
+    {
+      id: 1,
+      imgPath: TravelWeb,
+      title: 'TravelWebsite',
+      description: 'A travel planing website which built with ReactJS.',
+      projectLink: 'https://tarikbulbul-travelwebsite.netlify.app/',
+      codeLink: 'https://github.com/tarikbulbul/Travel-Website/',
+    },
+    {
+      id: 2,
+      imgPath: DefiWeb,
+      title: 'DefiWebsite',
+      description: 'A Defi website which built with ReactJS, react-icons.',
+      projectLink: 'https://tarikbulbul-defiwebsite.netlify.app/',
+      codeLink: 'https://github.com/tarikbulbul/Defi-Website/',
+    },
+    {
+      id: 3,
+      imgPath: RealEstateWeb,
+      title: 'RealEstateWebsite',
+      description: 'A real estate website which built with ReactJS, styled-components.',
+      projectLink: 'https://tarikbulbul-realestate.netlify.app/',
+      codeLink: 'https://github.com/tarikbulbul/Real-Estate/',
+    },
+    {
+      id: 4,
+      imgPath: NetflixClone,
+      title: 'NetflixClone',
+      description: 'A Netflix Clone using React, Firebase & Material-UI..',
+      projectLink: 'https://netflix-clone-5ba8d.firebaseapp.com/',
+      codeLink: 'https://github.com/tarikbulbul/Netflix-Clone/',
+    },
+    {
+      id: 5,
+      imgPath: AirbnbClone,
+      title: 'Airbnb Clone',
+      description: 'This project is a clone of the Airbnb website, built using React and Material-UI. The goal of this project was to replicate the core features and design of Airbnb to practice my frontend development skills and gain hands-on experience with popular libraries.',
+      projectLink: 'https://airbnb-clone-41687.firebaseapp.com/',
+      codeLink: 'https://github.com/tarikbulbul/Airbnb-Clone/',
+    },
+    {
+      id: 6,
+      imgPath: ReactMovieApp,
+      title: 'React Movie App',
+      description: 'This project contains a movie application that I created using React. The application utilizes the Open Movie Database (OMDb) API (omdbapi.com) to fetch movie data, allowing users to search for and view movie information.',
+      projectLink: 'https://react-movie-app-f93a8.firebaseapp.com/',
+      codeLink: 'https://github.com/tarikbulbul/react-movie-app/',
+    },
+    {
+      id: 7,
+      imgPath: ReactBudgetApp,
+      title: 'React Budget App',
+      description: 'This project includes a budget calculator web application developed using React and the Context API. The application is designed to assist users in managing their income and expenses. Through the use of the Context API, data sharing and management between components are facilitated, enabling global state management for the application.',
+      projectLink: 'https://react-budget-app-a0e26.firebaseapp.com/',
+      codeLink: 'https://github.com/tarikbulbul/react-budget-app/',
+    },
+  ];
+  
+  export default projects;
