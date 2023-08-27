c0fb76bc0079307cd28f0f8bda942f29b811aa63
Tarik Bulbul <bulbultarik@hotmail.com>
2023-08-27 09:11:52 +0300

    Add loading screen animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Preloader from './components/Pre';
 import Router from './router';
 import './style.css';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
+  const [load, setLoad] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+  
   return (
       <>
-        <div className="App">
+        <Preloader load={load} />
+        <div className="App" id={load ? "no-scroll" : "scroll"}>
           <Router />;
         </div>
       </>
